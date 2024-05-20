186ef70be551cceccf3a71ff9d3980e6470923fc
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-20 12:05:33 +0900

    변수 재할당 리팩토링

--- 02/js/convert-result.js
+++ 02/js/convert-result.js
@@ -2,16 +2,16 @@
 // 섭씨 : celsius, 화씨 : fahrenheit
 
 // let fah = parseFloat(prompt("변환할 화씨 온도"));
-// 사용자에게 숫자로 받은 수치(실수)를 변수에 넣는다.
+// // 사용자에게 숫자로 받은 수치(실수)를 변수에 넣는다.
 
 // let cel;
 // // 값할당 안한상태
 
-// cel = ((fah - 32) / 1.8)
+// cel = ((fah - 32) / 1.8);
 // // .toFixed(n); 반올림하여 소수점 n자리까지 출력 
 // cel = cel.toFixed(1);
 // // alert(`화씨 ${fah}℉는 섭씨 ${cel}℃입니다.`);
-// // document.write(`화씨 ${fah}℉는 섭씨 ${cel}℃입니다.`)
+// document.write(`화씨 ${fah}℉는 섭씨 ${cel}℃입니다.`);
 
 
 
@@ -23,6 +23,5 @@
 let cel2 = Number(prompt("화씨로 변경할 궁금한 섭씨 온도를 적어주세요(숫자만)"));
 // 섭씨 온도 = (화씨온도 - 32) / 1.8
 // 화씨 온도 = (섭씨온도 * 1.8) + 32
-let fah2 = (cel2 * 1.8) + 32;
-fah2 = fah2.toFixed(2);
+let fah2 = ((cel2 * 1.8) + 32).toFixed(2);
 document.write(`당신이 입력한 섭씨 온도 ${cel2}℃는 화씨로 변환하면 ${fah2}℉ 입니다.`);
