a2f3ef58323e0c20298dc87a6191e545e86544ac
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-21 13:32:40 +0900

    p.106과 p.319 객체 접근법 추가

--- 03/js/forin.js
+++ 03/js/forin.js
@@ -9,20 +9,46 @@ const gitBook = {
 // 변수는 내가 짓는 이름이고, 객체 안에서 키:값으로된 쌍에서 키만 가지고 온다
 
 // 일반 객체에서 값을 가져오는 방법
-console.log(`${gitBook.pages}`); // 272
 
-// for (key in gitBook) {
-//   document.write(`${key}: `);
-//   // 객체 각 쌍안에서 키만 출력함
-//   // for.. in문에서 각 쌍안에서 키를가지고 오고 싶다면 ${변수명}을 써주면 된다
+console.log(`${gitBook.pages}`); // 객체에 직접적으로 접근, 272 
 
-//   document.write(`${gitBook[key]} <br><br>`);
-//   //객체 각 쌍안에서 값만 출력함
-//   // for.. in문에서 각 쌍안에서 값을 가지고 오고 싶다면 ${객체명[변수명]}을 써주면 된다
+// p.319 객체에 체계적으로 접근하는 방식
+console.log(`${Object.keys(gitBook)}`); // string으로 뱉음
+let keys = Object.keys(gitBook);
+console.log(keys); // 배열로 뱉음
+console.log(keys[3]);
+console.log(`-------------------`);
 
-//   // document.write(`${gitBook.pages}<br>`);
-//   // 객체에서 고정값만 추출(기존 객체에서 값가져오는 법), 반복횟수는 쌍 수만큼
-// }
+console.log(`${Object.values(gitBook)}`); // string으로 뱉음
+let values = Object.values(gitBook);
+console.log(values); // 배열로 뱉음
+console.log(values[3]);
+console.log(`-------------------`);
+
+console.log(`${Object.entries(gitBook)}`);
+let entries = Object.entries(gitBook);
+console.log(entries); // 배열로 뱉음
+console.log(entries[3]);
+console.log(entries[3][0]);
+console.log(`-------------------`);
+
+
+
+
+
+for (let key in gitBook) {
+  document.write(`${key}: `);
+  // 객체 각 쌍안에서 키만 출력함
+  // for.. in문에서 각 쌍안에서 키를가지고 오고 싶다면 ${변수명}을 써주면 된다
+
+  document.write(`${gitBook[key]} <br><br>`);
+  //객체 각 쌍안에서 값만 출력함
+  // for.. in문에서 각 쌍안에서 값을 가지고 오고 싶다면 ${객체명[변수명]}을 써주면 된다
+
+  // document.write(`${gitBook.pages}<br>`);
+  // document.write(`${gitBook["pages"]}<br>`);
+  // 객체에서 고정값만 추출(기존 객체에서 값가져오는 법), 반복횟수는 쌍 수만큼
+}
 
 
 
@@ -46,15 +72,15 @@ console.log(`${gitBook.pages}`); // 272
 // }
 
 
-const students = ["Park", "Kim", "Lee", "Kang"];
-console.log(students);
-// 혹시, 배열을 for...in으로 돌리면?
-for (let student in students) {
-  document.write(`${student}: `);
-  // 인덱스 번호 나옴, 키가없으니까!
-  document.write(`${students[student]}<br> `);
-  // 값이 도출이 됨
-}
+// const students = ["Park", "Kim", "Lee", "Kang"];
+// console.log(students);
+// // 혹시, 배열을 for...in으로 돌리면?
+// for (let student in students) {
+//   document.write(`${student}: `);
+//   // 인덱스 번호 나옴, 키가없으니까!
+//   document.write(`${students[student]}<br> `);
+//   // 값이 도출이 됨
+// }
 // 객체는 키:값, 배열은 [값,값]
 // 배열에게 키를 물으면 인덱스 번호, 값을 물으면 값
 // 데이터를 구성할 때, 정보의 디테일이 필요하면 객체로 만들기, 순서와 값만 필요하면 배열로 만들자
