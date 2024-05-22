5b3d92a9bdf62868fac821d237341bd7d421cdea
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-22 12:49:24 +0900

    실행문 변경

--- 04/js/add-3.js
+++ 04/js/add-3.js
@@ -9,9 +9,9 @@ function calcSum(n) {
   // return 다음줄에 나오는 실행문은 무시된다.
   console.log(`${calcSum()}`);
 }
-
-let num = parseInt(prompt("몇까지 더할까요?")); // 파라미터용 변수 설정
-console.log(`1부터 ${num}까지 더하면 ${calcSum(num)}입니다.${calcSum()}`);
+document.write(`${calcSum(100)}`);
+// let num = parseInt(prompt("몇까지 더할까요?")); // 파라미터용 변수 설정
+// console.log(`1부터 ${num}까지 더하면 ${calcSum(num)}입니다.${calcSum()}`);
 // 함수콜링하여 반환받은 값 표출
 
 /** return으로 내보내는 변수, 밖에서 쓸수 있나요? ㄴㄴ
