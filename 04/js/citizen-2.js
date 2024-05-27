36948fec2d00ed7378e2df5ec0292de0400a01bf
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-27 10:51:36 +0900

    함수에 리턴이 필요한 이유, 콜백에 쓰기 위하여!

--- 04/js/citizen-2.js
+++ 04/js/citizen-2.js
@@ -1,8 +1,11 @@
+// 함수가 다른함수의 인자로 쓰인다는 말은 해당 함수의 return값이 다른함수에게 전달 된다는 뜻이다.
 function hello() {
   return "안녕하세요?";
+  // console.log("안녕하세요?"); // 실행문
 }
 function bye() {
   return "안녕히 가세요.";
+  // console.log("안녕히 가세요."); // 실행문
 }
 
 function userCheck(name, fn) {
@@ -10,5 +13,8 @@ function userCheck(name, fn) {
   // 파라미터 2개를 받아서 name은 변수로, fn은 함수로 실행해라 
 }
 
-userCheck("홍길동", hello);  // 홍길동님, 안녕하세요?
+userCheck("홍길동", hello);
+// 올바르게 리턴을 받으면 홍길동님, 안녕하세요?가 찍힘.
+// 리턴이 없는 함수를 인자로 받으면 함수로 인지를 못해서 undefined로 뜨고, 해당 함수 안에있는 실행문은 걍 실행함.
+// 홍길동님 undefined와 안녕히 가세요. 가 순차적으로 실행되는 것이다.
 userCheck("도레미", bye);  // 도레미님, 안녕히 가세요.
