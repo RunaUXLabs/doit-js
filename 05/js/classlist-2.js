f9742fbe67b31addafd34c71f65637a3df40428f
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-27 15:45:24 +0900

    classList.contains(클래스명) 반영범위 주석 변경

--- 05/js/classlist-2.js
+++ 05/js/classlist-2.js
@@ -5,7 +5,13 @@ const title = document.querySelector("#title");
 // }
 
 // classList.contains(클래스명) 소유여부를 확인, 가지고있으면 true
-// 한가지 클래스만 소유여부를 확인할 땐 .toggle()이 편하고, 여러클래스를 확인해야한다면 .contains()로 물어보자
+// 클래스의 소유상태를 가지고 원하는 클래스을 넣었다/뺏다 할꺼면 .toggle()
+
+// 클래스의 소유상태를 가지고 광범위한 대상의 변화를 이끌어 낼 거라면 .contains()를 활용하여 조건문의 조건으로 활용하자. 그후에 하고싶은 실행문 넣기
+
+// css에서 :has() 선택자가 있음, 내 자식중에 해당 클래스를 갖고있는 애가 있다면~~~ 이라면서 조건 짤 수 있음.
+// https://developer.mozilla.org/en-US/docs/Web/CSS/:has
+
 title.onclick = () => {
   if (!title.classList.contains("clicked")) {
     // clicked를 안가지고 있으면
