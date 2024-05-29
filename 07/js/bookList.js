4356e192eb1f4151ec72a2c0e409020f5919e7ab
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-29 13:32:30 +0900

    포커스 추가

--- 07/js/bookList.js
+++ 07/js/bookList.js
@@ -2,27 +2,29 @@
 
 // 저장하기 버튼을 누르면
 // 버튼의 원래 용도는 자료를 넘기고 새로고침을 한다. 그래서 입력후 버튼을 클릭하면 화면의 모든 내용 사라지는게 원래 기능.
-save.addEventListener('click', (e)=>{
+save.addEventListener('click', (e) => {
     e.preventDefault();
-    // save버튼의 기본기능 막기
+    // save버튼의 기본기능 막기, 이벤트 객체(파라미터로 밀어넣음)에서 기본기능에 접근하여 막아줘!
+
     // bookList에 넣을 li만들기
-    const li = document.createElement('li')
-    // li에 백팃을 사용해 태그째로 밀어넣기
+    const li = document.createElement('li');
+    // li에 백팃을 사용해 태그째로 밀어넣기(퍼블리싱 단계에서 css 다 해놓기)
     li.innerHTML = `
         ${title.value} - ${author.value}
         <span class="delButton">삭제</span>
     `;
-    bookList.appendChild(li)
-    
-    // 다른내용 받아야 하니까 폼 공백으로 돌리기
-    title.value = ""
-    author.value = ""
+    bookList.appendChild(li);
+
+    // 다른내용 받아야 하니까 폼 공백으로 돌리고, 포커스 이동하기
+    title.value = "";
+    author.value = "";
+    title.focus();
 
     // 삭제버튼 기능 만들기, 추가가 된 후에 삭제를 해야하므로 안에다 작성한다.
-    const delButtons = document.querySelectorAll('.delButton')
-    for(let button of delButtons){
-        button.addEventListener('click',function(){
-            this.parentNode.remove()
-        })
+    const delButtons = document.querySelectorAll('.delButton');
+    for (let button of delButtons) {
+        button.addEventListener('click', function () {
+            this.parentNode.remove();
+        });
     }
-})
+});
