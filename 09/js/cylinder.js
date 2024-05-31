26a421a50d23fcfdcf2559b3ac4d8f63f7ef81bb
RunaUXLabs <runa.uxlabs@gmail.com>
2024-05-31 14:20:51 +0900

    UX개선

--- 09/js/cylinder.js
+++ 09/js/cylinder.js
@@ -32,30 +32,36 @@ btn.addEventListener('click', (e) => {
     e.preventDefault(); // 버튼 새로고침 막기
 
     // 값 추출
-    const diamiter = document.querySelector('#cyl-diameter').value;
-    const height = document.querySelector('#cyl-height').value;
+    const diamiter = document.querySelector('#cyl-diameter');
+    const height = document.querySelector('#cyl-height');
 
     // 교재 답
-    // if(diamiter === "" || height === ""){
-    //     result.textContent = '값을 입력해주시오'
+    // if (diamiter === "" || height === "") {
+    //     result.textContent = '값을 입력해주시오';
     // } else {
-    //     let cylinder = new Cylinder(parseInt(diamiter), parseInt(height))
-    //     result.textContent = `원기둥의 부피는 ${cylinder.getVolume()}입니다.`
+    //     let cylinder = new Cylinder(parseInt(diamiter), parseInt(height));
+    //     result.textContent = `원기둥의 부피는 ${cylinder.getVolume()}입니다.`;
     // }
 
     // 응용답
-    const Int_diamiter = parseInt(diamiter);
-    const Int_height = parseInt(height);
+    const Int_diamiter = parseInt(diamiter.value);
+    const Int_height = parseInt(height.value);
     if (isNaN(Int_diamiter)) {
         result.textContent = '지름에 숫자값을 입력해주시오';
         diamiter.value = '';
+        diamiter.focus();
+        return false;
     }
     if (isNaN(Int_height)) {
         result.textContent = '높이에 숫자값을 입력해주시오';
         height.value = '';
+        height.focus();
+        return false;
     }
     if (diamiter === "" || height === "") {
         result.textContent = '값을 입력해주시오';
+        diamiter.focus();
+        return false;
     } else if (!isNaN(Int_diamiter) && !isNaN(Int_height)) {
         let cylinder = new Cylinder(Int_diamiter, Int_height);
         result.textContent = `원기둥의 부피는 ${cylinder.getVolume()}입니다.`;
