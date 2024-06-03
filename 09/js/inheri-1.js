5773bd90a4f8d9b75944f9fed59df4c659ad9a39
RunaUXLabs <runa.uxlabs@gmail.com>
2024-06-03 13:35:00 +0900

    .call()을 활용한 생성자함수 프로토타입 상속

--- 09/js/inheri-1.js
+++ 09/js/inheri-1.js
@@ -16,12 +16,12 @@ console.log(book1);
 console.log(book1.buy());
 
 // 프로토타입이 추가 된 후 새로운 인스턴스를 만들었을때 모든 요소가 다 출력된다.
-const book2 = new Book('css기초', 3500);
-console.log(book2);
-console.log(book2.buy());
+// const book2 = new Book('css기초', 3500);
+// console.log(book2);
+// console.log(book2.buy());
 
 
-/**
+/**p.330 4번내용
  * 기존 생성자 함수를 재활용하고 싶을때 .call()을 사용한다
  * function 신규생성자함수명(기존파라미터들, 새파라미터들){
  *   기존생성자함수.call(this, 기존파라미터들)
@@ -33,19 +33,25 @@ console.log(book2.buy());
 
 function Textbook(title, price, major) {
     Book.call(this, title, price);
+    // this는 Textbook으로 찍으려는 신규 인스턴스, Book에서 title, price가 선언된거 그대로 가져온다
     this.major = major;
 }
 
 const book3 = new Textbook('자바스크립트', 3000, '컴퓨터공학');
 console.log(book3);
 // console.log(book3.buy());
+// buy()는 가져오지 않았으므로 사용할 수 없는 함수라고 에러가 난다.
 
+
+// Book()을 참조하는 Textbook()에 프로토타입으로 buyTextbook 메서드를 추가한다.
 Textbook.prototype.buyTextbook = function () {
     console.log(`${this.major} 전공서적, ${this.title}을 구매했습니다`);
 };
 
 console.log(book3.buyTextbook());
-// console.log(book3.buy()); // book3.buy is not a function 내가 가져온것만 뜸 title, price만 가져옴
+// buyTextbook 메서드가 추가되기 전에 만들어진 book3에게 해당 메서드를 물어보면 실행해준다.
+// console.log(book3.buy());
+// book3.buy is not a function, 가져오지 않은 buy()는 실행할 수 없다.
 
 
 // 프로토타입으로 따로 관리되는것을 연결하기(귀속은 아님, 따로관리됨)
